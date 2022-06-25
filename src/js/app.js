3c4f796d317e3f1756ec10cf74f3252d39b527de
Sorokovsky <Sorokovskys@ukr.net>
2022-06-25 18:48:50 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,5 @@
-import { isWebp } from "./modules/functions.js";
-isWebp();
+import { isWebp, burgerMenu } from "./modules/functions.js";
 const burger = document.querySelector(".burger");
 const menu = document.querySelector('.header__menu');
-if (burger) {
-    burger.addEventListener("click", (e) => {
-        if(menu){
-            if (menu.classList.contains('_active')) {
-                burger.classList.remove('_active');
-                burger.children[0].classList.remove('_active');
-                menu.classList.remove('_active');
-                document.body.classList.remove('_lock');
-            }else{
-                burger.classList.add('_active');
-                burger.children[0].classList.add('_active');
-                menu.classList.add('_active');
-                document.body.classList.add('_lock');
-            }
-        }
-    });
-}
+isWebp();
+burgerMenu(burger, menu);
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -17,3 +17,31 @@ export function isWebp(){
         }
     });
 }
+export function burgerMenu(burger, menu){
+    toggleMenu(burger, menu);
+}
+export function toggleMenu(burger, menu){
+    if (burger) {
+        burger.addEventListener("click", (e) => {
+            if(menu){
+                if (menu.classList.contains('_active')) {
+                    closeMenu(burger, menu);
+                }else{
+                    openMenu(burger, menu);
+                }
+            }
+        });
+    }
+}
+export function openMenu(burger, menu){
+    burger.classList.add('_active');
+    burger.children[0].classList.add('_active');
+    menu.classList.add('_active');
+    document.body.classList.add('_lock');
+}
+export function closeMenu(burger, menu){
+    burger.classList.remove('_active');
+    burger.children[0].classList.remove('_active');
+    menu.classList.remove('_active');
+    document.body.classList.remove('_lock');     
+}
